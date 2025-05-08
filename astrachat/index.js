afa15199dd73543a799fcb366917bd229c0c4ac5
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2025-05-08 19:42:00 -0300

    change data dir

--- astrachat/index.js
+++ astrachat/index.js
@@ -2,9 +2,9 @@ import { createAstrachat } from "@bitxenia/astrachat";
 import { FsBlockstore } from "blockstore-fs";
 import { FsDatastore } from "datastore-fs";
 
-const DATASTORE_DIR = "/app/data/datastore";
-const BLOCKSTORE_DIR = "/app/data/blockstore";
-const ORBITDB_DIR = "/app/data/bitxenia-chat";
+const DATASTORE_DIR = "/app/data/astrachat/datastore";
+const BLOCKSTORE_DIR = "/app/data/astrachat/blockstore";
+const ORBITDB_DIR = "/app/data/astrachat";
 
 const node = await createAstrachat({
   chatSpace: "bitxenia-chat",
